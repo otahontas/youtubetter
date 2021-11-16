691a8416c33f1aa6cd3f778ebdd852c9b831dc10
Leo Varis <leovaris04@gmail.com>
2021-11-16 23:55:52 +0200

    setup client project, #3, #13, #10

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,11 @@
-function App() {
+import YoutubeEmbed from "./YoutubeEmbed"
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <YoutubeEmbed embedId="rokGy0huYEA" />
+    </>
   );
 }
 
-export default App;
+export default App
