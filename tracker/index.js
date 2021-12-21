6fed277a2027af6ab12db616f141158ff503da32
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-12-21 19:38:06 +0200

    Comment stuff

--- tracker/index.js
+++ tracker/index.js
@@ -1,42 +1,56 @@
 import WebSocket, { WebSocketServer } from "ws";
 import queue from "queue";
+require("dotenv").config();
 
-// Set to true to enable debugging
-const DEBUG = false;
+// Configuration. Give env variables in either .env file or via settings environment
+// variables
+const DEBUG = process.env.DEBUG || false;
+const PORT = process.env.PORT || 8080;
 
-const wss = new WebSocketServer({ port: 8080 });
-const q = queue({ results: [] });
-// Log when stuff happens in queue
-q.on("success", (result) => console.log(`Succesfully finished job: ${result}`));
-q.on("end", () => console.log("No more stuff in queue"));
+const wss = new WebSocketServer({ port: PORT });
+const messageQueue = queue({ results: [] });
 
-q.start();
+// Functions to help debugging
+const timeOutRangeInSeconds = 10;
+const randomTimeout = () =>
+  Math.floor(Math.random() * timeOutRangeInSeconds * 1000);
+const waitForRandomTime = () => setTimeout(async () => {}, randomTimeout());
 
+// Log when something happens with messageQueue
+messageQueue.on("success", (result) =>
+  console.log(`Succesfully finished job: ${result}`)
+);
+messageQueue.on("end", () => console.log("No more stuff in queue"));
+
+// Start receiving messages
+messageQueue.start();
+
+// Send message to all connected clients
 const sendMessageToAll = (data, isBinary) => {
-  // wss clients is Set() so destructuring
-  const clientsConnected = [...wss.clients].reduce(
+  const allClientsConnected = [...wss.clients].reduce(
     (connected, client) => connected && client.readyState === WebSocket.OPEN,
     true
   );
-  if (clientsConnected) {
+
+  // Check if clients are connected, else push the message back to the queue
+  if (allClientsConnected) {
     wss.clients.forEach((client) => client.send(data, { binary: isBinary }));
   } else {
     // push message back to queue if clients are not ready
-    // maybe wait here?
-    q.push(sendMessageToAll(data));
+    messageQueue.push(sendMessageToAll(data));
   }
 };
 
-// Add received messages to queue
-const randomTimeout = () => Math.floor(Math.random() * 10000);
-const waitForRandomTime = () => setTimeout(async () => {}, randomTimeout());
-
+// Add event listeners to the connected websocket server
 wss.on("connection", (ws) => {
   ws.on("message", (data, isBinary) => {
-    console.log("got message", data.toString());
     if (DEBUG) {
+      // Wait for random time to simulate a slow connection
       waitForRandomTime();
     }
-    q.push(sendMessageToAll(data, isBinary));
+    console.log("got message", data.toString());
+    // Map each incoming message to a function which will send message to all clients
+    // and push the function to the queue
+    messageQueue.push(sendMessageToAll(data, isBinary));
   });
 });
