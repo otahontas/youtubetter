d985d71bce23d620188428a84a1b6bb6524ae96f
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-12-21 21:34:50 +0200

    Fix url

--- client/src/conf.js
+++ client/src/conf.js
@@ -1,4 +1,4 @@
 export const isProduction = process.env.NODE_ENV === "production";
 export const trackerUrl = isProduction
-  ? "ws://youtubetter.toska.cs.helsinki.fi"
+  ? "ws://youtubetter.toska.cs.helsinki.fi/tracker"
   : "ws://localhost:3000";
